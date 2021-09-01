6c75b18bacc5dbc81a32026d2b172a2817625d48
Kevin Johnson <kjohnson2827@gmail.com>
2021-08-31 20:02:24 -0400

    ShoppingList models completed. Service and controller unfinished

---
+++ GroceryList.Models/ShoppingListProperties.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryList.Models
+{
+    public class ShoppingListProperties
+    {
+        public List<int> Ingredients { get; set; }
+        public string StoreName { get; set; }
+
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        [Display(Name = "Modified")]
+        public DateTimeOffset ModifiedUtc { get; set; }
+    }
+}
---
+++ GroceryList.WebAPI/Controllers/ShoppingListController.cs
@@ -0,0 +1,56 @@
+﻿using GroceryList.Models;
+using GroceryList.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GroceryList.WebAPI.Controllers
+{
+    public class ShoppingListController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            ShoppingListService shoppingListService = CreateShoppingListService();
+            var ingredients = ShoppingListService.GetIngredients();
+            return Ok(ingredients);
+        }
+
+        public IHttpActionResult Post(ShoppingListCreate shoppingList)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState); ;
+
+            var service = CreateShoppingListService();
+
+            if (!service.CreateShoppingList(shoppingList))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Put(ShoppingListEdit shoppingList)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateShoppingListService();
+
+            if (!service.UpdateShoppingList(shoppingList))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateShoppingListService();
+            if (!service.DeleteShoppingList(id))
+                return InternalServerError();
+
+            return Ok();
+        }
+    }
+}
